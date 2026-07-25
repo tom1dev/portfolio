2c7547af924aa1e266b539f4bb4afd584a41e6b6
tom devonshire <tomma@MacBook-Pro.local>
2026-07-25 18:47:50 +1200

    contact stuff added

--- src/components/ContactMe.tsx
+++ src/components/ContactMe.tsx
@@ -1,9 +1,25 @@
 import "tailwindcss";
 function ContactMe(){
     return(
-    <>
-        Contact Me
-    </>
+    <div>
+        {/* Header */}
+        <h2>Contact Me</h2>
+        {/* Line items */}
+        <ul>
+            <li>
+                Email: tom@devonshire.co.nz
+            </li>
+            <li>
+                Linked-In: <a href="https://www.linkedin.com/in/tom1dev/" className="hover:text-blue-900">https://www.linkedin.com/in/tom1dev/</a> 
+            </li>
+            <li>
+                GitHub: <a href="https://github.com/tom1dev" className="hover:text-blue-900">https://github.com/tom1dev</a> 
+            </li>
+            <li>
+                Location: Auckland
+            </li>
+        </ul>
+    </div>
     );
 }
 
